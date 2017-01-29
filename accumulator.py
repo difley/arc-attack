a20529112e64a5d89d52146a062f421b160b55b8
Seth Difley <difley@gmail.com>
2017-01-29 10:32:45 -0600

    Converted closure to class.

--- accumulator.py
+++ accumulator.py
@@ -2,30 +2,34 @@ import scipy
 import arc_attack as curve
 
 
-def sample_accumulator(original_start_point, original_tangent_angle, config):
-    samples = []
-    state = {'point': original_start_point,
-             'angle': original_tangent_angle}
+class SampleAccumulator(object):
+    def __init__(self, original_start_point, original_tangent_angle, config):
+        self.samples = []
+        self.state = {'point': original_start_point,
+                      'angle': original_tangent_angle}
+        self.config = config
 
-    def add_sample(sample_type, parameters):
-        nonlocal state
+
+    def add_sample(self, sample_type, parameters):
         if sample_type == 'line':
-            state = line(state['point'], state['angle'],
-                         parameters['distance'])
+            self.state = self._line(self.state['point'], self.state['angle'],
+                                    parameters['distance'])
         elif sample_type == 'arc':
-            state = arc(parameters['radius'], parameters['central_angle'],
-                        parameters['clockwise'], state['point'],
-                        state['angle'])
+            self.state = self._arc(parameters['radius'], parameters['central_angle'],
+                                   parameters['clockwise'], self.state['point'],
+                                   self.state['angle'])
 
-    def line(origin, tangent_angle, distance):
+
+    def _line(self, origin, tangent_angle, distance):
         parametrized_function = curve.generate_parametrized_line(origin, tangent_angle, distance)
-        samples.append(curve.sampler(parametrized_function, 0.0, 1.0,
-                                     config['line_sample_count']))
+        self.samples.append(curve.sampler(parametrized_function, 0.0, 1.0,
+                                     self.config['line_sample_count']))
         end_point = parametrized_function(1.0)
         return {'point': end_point,
                 'angle': tangent_angle}
 
-    def arc(radius, central_angle, clockwise,
+
+    def _arc(self, radius, central_angle, clockwise,
                       start_point, start_tangent_angle):
         start_angle = curve.normalize_angle(
                           curve.get_radial_angle_from_arc_tangent(start_tangent_angle,
@@ -38,8 +42,8 @@ def sample_accumulator(original_start_point, original_tangent_angle, config):
                   'stop': stop_angle}
         center = curve.get_arc_center(start_point, radius, start_angle)
         parametrized_function = curve.generate_parametrized_arc(center, angles, radius)
-        samples.append(curve.sampler(parametrized_function, 0.0, 1.0,
-                                     config['arc_sample_count']))
+        self.samples.append(curve.sampler(parametrized_function, 0.0, 1.0,
+                                     self.config['arc_sample_count']))
         end_point = parametrized_function(1.0)
         tangent_angle = curve.normalize_angle(
                       curve.get_arc_tangent_angle_from_radial_angle(angles['stop'],
@@ -48,9 +52,5 @@ def sample_accumulator(original_start_point, original_tangent_angle, config):
                 'angle': tangent_angle}
 
 
-
-    def return_samples():
-        return samples
-
-    return {'add_sample': add_sample,
-            'return_samples': return_samples}
+    def return_samples(self):
+        return self.samples
--- demo.py
+++ demo.py
@@ -1,26 +1,26 @@
 import scipy
 from arc_attack import write_samples_to_file
-from api import sample_accumulator
+from accumulator import SampleAccumulator
 
 
 def main():
     data_filename = 'output.out'
-    accumulator = sample_accumulator({'x': 0.0, 'y': 0.0},
+    accumulator = SampleAccumulator({'x': 0.0, 'y': 0.0},
                                       scipy.pi*3./2.,
                                       {'line_sample_count': 2,
                                        'arc_sample_count': 10})
-    accumulator['add_sample']('line', {'distance': 1.0})
-    accumulator['add_sample']('arc', {'radius': 1.0,
+    accumulator.add_sample('line', {'distance': 1.0})
+    accumulator.add_sample('arc', {'radius': 1.0,
                                    'central_angle': scipy.pi,
                                    'clockwise': False})
-    accumulator['add_sample']('line', {'distance': 0.5})
-    accumulator['add_sample']('arc', {'radius': 0.5,
+    accumulator.add_sample('line', {'distance': 0.5})
+    accumulator.add_sample('arc', {'radius': 0.5,
                                    'central_angle': scipy.pi*5./3.,
                                    'clockwise': True})
-    accumulator['add_sample']('arc', {'radius': 0.75,
+    accumulator.add_sample('arc', {'radius': 0.75,
                                    'central_angle': scipy.pi*5./3.,
                                    'clockwise': False})
-    samples = accumulator['return_samples']()
+    samples = accumulator.return_samples()
     write_samples_to_file(samples, data_filename)
 
 
