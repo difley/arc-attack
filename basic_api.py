7b830117ae38544a7797ecae69b70148526892b6
Seth Difley <difley@gmail.com>
2017-01-29 12:43:40 -0600

    Added basic API tests.

--- basic_api.py
+++ basic_api.py
@@ -46,36 +46,6 @@ def normalize_angle(angle):
     return scipy.fmod(angle, scipy.pi*2.)
 
 
-def random_generator():
-    while True:
-        yield scipy.random.random()
-
-
-def get_line_distance(generator=None):
-    if not generator:
-        generator = random_generator()
-    minimum_line_distance = 0.3
-    line_scale_factor = 1.0
-    distance = next(generator)*line_scale_factor + minimum_line_distance
-    return distance
-
-
-def get_arc_radius(generator=None):
-    if not generator:
-        generator = random_generator()
-    minimum_radius = 0.2
-    radius_scaling_factor = 0.3
-    radius = next(generator)*radius_scaling_factor + minimum_radius
-    return radius
-
-
-def get_angle_end(angle, generator=None):
-    if not generator:
-        generator = random_generator()
-    angle_end = angle + next(generator)*2.*scipy.pi
-    return angle_end
-
-
 def sampler(parametrized_function, start_t, stop_t, point_count):
     return map(lambda t: parametrized_function(t),
                scipy.linspace(start_t, stop_t, point_count))
--- tests/test_basic_api.py
+++ tests/test_basic_api.py
@@ -34,5 +34,79 @@ class TestGetArcCenter(unittest.TestCase):
         self.assertAlmostEqual(center['y'], 2.0)
 
 
+class TestGetParametrizedLine(unittest.TestCase):
+    def test_vertical_line(self):
+        origin = {'x': 0.0, 'y': 0.0}
+        tangent_angle = scipy.pi*3./2.
+        distance = 1.0
+        parametrized_function = basic_api.generate_parametrized_line(origin, tangent_angle, distance)
+        end_point = parametrized_function(1.0)
+        self.assertAlmostEqual(end_point['x'], 0.0)
+        self.assertAlmostEqual(end_point['y'], -1.0)
+
+
+    def test_horizontal_line(self):
+        origin = {'x': 0.0, 'y': 0.0}
+        tangent_angle = scipy.pi
+        distance = 1.0
+        parametrized_function = basic_api.generate_parametrized_line(origin, tangent_angle, distance)
+        end_point = parametrized_function(1.0)
+        self.assertAlmostEqual(end_point['x'], -1.0)
+        self.assertAlmostEqual(end_point['y'], 0.0)
+
+
+    def test_diagonal_line(self):
+        origin = {'x': 0.0, 'y': 0.0}
+        tangent_angle = scipy.pi*3./4.
+        distance = scipy.sqrt(2.0)
+        parametrized_function = basic_api.generate_parametrized_line(origin, tangent_angle, distance)
+        end_point = parametrized_function(1.0)
+        self.assertAlmostEqual(end_point['x'], -1.0)
+        self.assertAlmostEqual(end_point['y'], 1.0)
+
+
+class TestGetParametrizedArc(unittest.TestCase):
+    def test_full_circle(self):
+        angles = {'start': 0.0,
+                  'stop': scipy.pi*2.}
+        radius = 2.0
+        center = {'x': 3.0, 'y': -5.0}
+        parametrized_function = basic_api.generate_parametrized_arc(center, angles, radius)
+        start_point = parametrized_function(0.0)
+        self.assertAlmostEqual(start_point['x'], 5.0)
+        self.assertAlmostEqual(start_point['y'], -5.0)
+        end_point = parametrized_function(1.0)
+        self.assertAlmostEqual(end_point['x'], 5.0)
+        self.assertAlmostEqual(end_point['y'], -5.0)
+
+
+    def test_arc_segment(self):
+        angles = {'start': 0.0,
+                  'stop': scipy.pi/4.}
+        radius = scipy.sqrt(2.0)
+        center = {'x': 3.0, 'y': -5.0}
+        parametrized_function = basic_api.generate_parametrized_arc(center, angles, radius)
+        start_point = parametrized_function(0.0)
+        self.assertAlmostEqual(start_point['x'], 4.4142135623)
+        self.assertAlmostEqual(start_point['y'], -5.0)
+        end_point = parametrized_function(1.0)
+        self.assertAlmostEqual(end_point['x'], 4.0)
+        self.assertAlmostEqual(end_point['y'], -4.0)
+
+
+    def test_reversed_arc_segment(self):
+        angles = {'start': scipy.pi/4.,
+                  'stop': 0.0}
+        radius = scipy.sqrt(2.0)
+        center = {'x': 3.0, 'y': -5.0}
+        parametrized_function = basic_api.generate_parametrized_arc(center, angles, radius)
+        start_point = parametrized_function(0.0)
+        self.assertAlmostEqual(start_point['x'], 4.0)
+        self.assertAlmostEqual(start_point['y'], -4.0)
+        end_point = parametrized_function(1.0)
+        self.assertAlmostEqual(end_point['x'], 4.4142135623)
+        self.assertAlmostEqual(end_point['y'], -5.0)
+
+
 if __name__ == '__main__':
     unittest.main()
